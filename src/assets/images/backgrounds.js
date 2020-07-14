f586e918cc536780a3b6b88195e0b55e76723c8b
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2020-07-14 22:16:32 +0800

    add images urls

---
+++ src/assets/images/backgrounds.js
@@ -0,0 +1,3 @@
+export const desktopBannerBG = 'http://via.placeholder.com/1920x650';
+export const mobileBannerBG = 'http://via.placeholder.com/600x600';
+export const contentBG = 'http://via.placeholder.com/400x300';
